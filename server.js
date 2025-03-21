6de5c16da250a33ce4ebf2852dfaff18cfc06fb7
Prithvi Singh <prithvisingh@Prithvis-MacBook-Air.local>
2025-03-21 19:40:06 +0530

    connected mongodb

--- server.js
+++ server.js
@@ -1,34 +1,23 @@
 require('dotenv').config();  // Make sure this line is at the top
 const express = require("express");
-const { MongoClient } = require("mongodb");
+const mongoose = require('mongoose')
 const app = express();
 const port = 3000;
 
-let dbClient;
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('✅ MongoDB Connected'))
+.catch(err => console.error('❌ MongoDB Connection Error:', err));
 
-// Connect to MongoDB
-MongoClient.connect(process.env.MONGODB_URI)
-  .then(client => {
-    dbClient = client;
-    console.log("Connected to MongoDB");
-  })
-  .catch(error => {
-    console.error("Error connecting to MongoDB:", error);
-  });
 
 // Simple ping route
 app.get("/ping", (req, res) => {
   res.send("Pong!");
 });
 
-// Home route to check MongoDB connection status
-app.get("/", (req, res) => {
-  if (dbClient) {
-    res.status(200).send("Connected to MongoDB");
-  } else {
-    res.status(500).send("Failed to connect to MongoDB");
-  }
-});
+
 
 // Start the server
 app.listen(port, () => {
